aa4fcdcc973712cc36af22bac9baa2065caba274
Henry TANG <100391137+HenryTANG0520@users.noreply.github.com>
2023-05-08 13:53:42 +1000

    修改了轮播图片，现在轮播图片可用

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     let slides = document.querySelectorAll(".slide");
-//     let currentSlide = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    let slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
   
-//     setInterval(function () {
-//       slides[currentSlide].classList.remove("active");
-//       currentSlide = (currentSlide + 1) % slides.length;
-//       slides[currentSlide].classList.add("active");
-//     }, 5000); // 5000 毫秒（5 秒）切换一次背景图片
-//   });
+    setInterval(function () {
+      slides[currentSlide].classList.remove("active");
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].classList.add("active");
+    }, 5000); // 5000 毫秒（5 秒）切换一次背景图片
+  });
   
