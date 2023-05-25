0bbb72aa312bd93ac014f674ae3e81321813c05f
Henry TANG <100391137+HenryTANG0520@users.noreply.github.com>
2023-05-26 01:28:33 +1000

    添加了3个饼图

--- chart.js
+++ chart.js
@@ -58,3 +58,74 @@ function getTrace(data, species) {
 
 // Initialize the plot with default value (0)
 updatePlot(0);
+
+
+
+
+// 鲸鱼饼图
+// 数据来源：https://a-z-animals.com/blog/how-do-whales-die-7-common-causes-of-death-for-whales/
+var data = [{
+    values: [20, 20, 20, 10, 10, 10, 10],
+    labels: ['Ship Strikes', 'Pollution', 'Beaching', 'Predators', 'Illness', 'Starvation', 'Hunting'],
+    type: 'pie'
+  }];
+  
+  var layout = {
+    title: 'Main Causes of Whale Deaths',
+    height: 400,
+    width: 300,
+    legend: {
+        x: 0.5,
+        y: -0.1,
+        xanchor: 'center',
+        yanchor: 'top'
+      }
+  };
+  
+  Plotly.newPlot('chart-pie-1', data, layout);
+  
+// 大象饼图
+
+var data = [{
+    values: [33.1, 31.5, 19.9],
+    labels: ['Natural Causes', 'Ivory Poaching', 'Human-Elephant Conflicts'],
+    type: 'pie'
+  }];
+  
+  var layout = {
+    title: 'Main Causes of Elephant Deaths',
+    height: 400,
+    width: 300,
+    legend: {
+        x: 0.5,
+        y: -0.1,
+        xanchor: 'center',
+        yanchor: 'top'
+      }
+  };
+  
+  Plotly.newPlot('chart-pie-2', data, layout);
+// 犀牛饼图
+// 数据来源：https://en.wikipedia.org/wiki/Rhinoceros_poaching_in_Southern_Africa
+// 
+
+var data_rhino = [{
+    values: [80, 20],
+    labels: ['Poaching for horns', 'Uncontrolled hunting'],
+    type: 'pie'
+  }];
+  
+  var layout_rhino = {
+    title: 'Main Causes of Rhino Deaths',
+    height: 400,
+    width: 300,
+    legend: {
+        x: 0.5,
+        y: -0.1,
+        xanchor: 'center',
+        yanchor: 'top'
+      }
+  };
+  
+  Plotly.newPlot('chart-pie-3', data_rhino, layout_rhino);
+  
