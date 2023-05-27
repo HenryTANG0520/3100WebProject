9c14cf7a5d4a0366e310267c844f28e728feb069
Henry TANG <100391137+HenryTANG0520@users.noreply.github.com>
2023-05-27 15:08:58 +1000

    优化了js代码的可读性

--- chart.js
+++ chart.js
@@ -1,39 +1,33 @@
 // Path to CSV file
-const csvFilePath = 'data/cleaned-data/line-chart-1.csv';
+const csvFilePath1 = 'data/cleaned-data/line-chart-1.csv';
+const csvFilePath2 = 'data/cleaned-data/southern-white-rhinos.csv';
 
-// Function to read CSV and parse data
-async function readLineChartCSV() {
-  const response = await fetch(csvFilePath);
-  const csvContent = await response.text();
-  const data = Plotly.d3.csv.parse(csvContent);
-  return data;
-}
+// Update plot based on button value
+function updatePlot(buttonValue) {
+  Plotly.d3.csv(csvFilePath1, async function(data) {
 
-// Function to update plot based on button value
-async function updatePlot(buttonValue) {
-  const data = await readLineChartCSV();
+    const speciesMap = { 1: 'Northen White Rhino', 2: 'Africa Elephant', 3: 'Whale' };
+    const species = speciesMap[buttonValue];
 
-  const speciesMap = { 1: 'Northen White Rhino', 2: 'Africa Elephant', 3: 'Whale' };
-  const species = speciesMap[buttonValue];
+    const plotData = [];
 
-  const plotData = [];
-
-  if (species) {
-    plotData.push(getTrace(data, species));
-  } else {
-    for (let key in speciesMap) {
-      plotData.push(getTrace(data, speciesMap[key]));
+    if (species) {
+      plotData.push(getTrace(data, species));
+    } else {
+      for (let key in speciesMap) {
+        plotData.push(getTrace(data, speciesMap[key]));
+      }
     }
-  }
 
-  const layout = {
-    title: '物种数量变化折线图',
-    xaxis: { title: '年份' },
-    yaxis: { title: '数量' },
-    height: '100%' // 图表高度设置为 '100%'，使其占满容器
-  };
+    const layout = {
+      title: '物种数量变化折线图',
+      xaxis: { title: '年份' },
+      yaxis: { title: '数量' },
+      height: '100%' // 图表高度设置为 '100%'，使其占满容器
+    };
 
-  Plotly.react('line-chart-1', plotData, layout);
+    Plotly.react('line-chart-1', plotData, layout);
+  });
 }
 
 // Function to generate trace for a species
@@ -59,130 +53,112 @@ function getTrace(data, species) {
 // Initialize the plot with default value (0)
 updatePlot(0);
 
-
-
-
 // 鲸鱼饼图
-// 数据来源：https://a-z-animals.com/blog/how-do-whales-die-7-common-causes-of-death-for-whales/
 var data = [{
     values: [20, 20, 20, 10, 10, 10, 10],
     labels: ['Ship Strikes', 'Pollution', 'Beaching', 'Predators', 'Illness', 'Starvation', 'Hunting'],
     type: 'pie',
     marker:{
-      colors:['#8a8a8a','#ababab','#c0c0c0','#cbcbcb','#dedede','#f0f0f0','darkred']
+      colors:['#8a8a8a','#ababab','#c0c0c0','#cbcbcb','#dedede','#f0f0f0','#B22222']
     }
-  }];
+}];
   
-  var layout = {
-    title: 'Main Causes of Whale Deaths',
-    height: 400,
-    width: 300,
-    legend: {
-        x: 0.5,
-        y: -0.1,
-        xanchor: 'center',
-        yanchor: 'top'
-      }
-  };
+var layout = {
+  title: 'Main Causes of Whale Deaths',
+  height: 400,
+  width: 300,
+  legend: {
+      x: 0.5,
+      y: -0.1,
+      xanchor: 'center',
+      yanchor: 'top'
+    }
+};
   
-  Plotly.newPlot('chart-pie-1', data, layout);
-  
-// 大象饼图
+Plotly.newPlot('chart-pie-1', data, layout);
 
+// 大象饼图
 var data = [{
     values: [33.1, 31.5, 19.9],
     labels: ['Natural Causes', 'Ivory Poaching', 'Human-Elephant Conflicts'],
     type: 'pie',
     marker:{
-      colors:['#aaa','darkred','#ccc']
+      colors:['#aaa','#B22222','#ccc']
     }
-  }];
+}];
   
-  var layout = {
-    title: 'Main Causes of Elephant Deaths',
-    height: 400,
-    width: 300,
-    legend: {
-        x: 0.5,
-        y: -0.1,
-        xanchor: 'center',
-        yanchor: 'top'
-      }
-  };
+var layout = {
+  title: 'Main Causes of Elephant Deaths',
+  height: 400,
+  width: 300,
+  legend: {
+      x: 0.5,
+      y: -0.1,
+      xanchor: 'center',
+      yanchor: 'top'
+    }
+};
   
-  Plotly.newPlot('chart-pie-2', data, layout);
-// 犀牛饼图
-// 数据来源：https://en.wikipedia.org/wiki/Rhinoceros_poaching_in_Southern_Africa
-// 
+Plotly.newPlot('chart-pie-2', data, layout);
 
+// 犀牛饼图
 var data_rhino = [{
     values: [80, 20],
     labels: ['Poaching for horns', 'Uncontrolled hunting'],
     type: 'pie',
     marker:{
-      colors:['darkred','a0a0a0']
+      colors:['#B22222','a0a0a0']
     }
-  }];
+}];
   
-  var layout_rhino = {
-    title: 'Main Causes of Rhino Deaths',
-    height: 400,
-    width: 300,
-    legend: {
-        x: 0.5,
-        y: -0.1,
-        xanchor: 'center',
-        yanchor: 'top'
+var layout_rhino = {
+  title: 'Main Causes of Rhino Deaths',
+  height: 400,
+  width: 300,
+  legend: {
+      x: 0.5,
+      y: -0.1,
+      xanchor: 'center',
+      yanchor: 'top'
+    }
+};
+  
+Plotly.newPlot('chart-pie-3', data_rhino, layout_rhino);
+
+// Southern White Rhino recovery data
+function createLineChart() {
+  Plotly.d3.csv(csvFilePath2, function(data) {
+
+    const x = data.map(row => row.Year);
+    const y = data.map(row => +row['Southern White Rhino population (AfRSG & other sources, 2019)']);
+
+    const trace = {
+      x: x,
+      y: y,
+      mode: 'lines+markers',
+      line: {
+        color: '#F5DEB3'
+      },
+      marker: {
+        color: '#D2B48C',
+        size: 6
+      },
+      name: 'Southern White Rhino population'
+    };
+
+    const layout = {
+      title: 'Southern White Rhino Population',
+      xaxis: {
+        title: 'Year'
+      },
+      yaxis: {
+        title: 'Population'
       }
-  };
-  
-  Plotly.newPlot('chart-pie-3', data_rhino, layout_rhino);
-  
-// 南方犀牛恢复数据
+    };
 
-
-// 引入CSV文件并解析数据
-async function loadData() {
-  const csvFilePath = 'data/cleaned-data/southern-white-rhinos.csv';
-  const response = await fetch(csvFilePath);
-  const csvContent = await response.text();
-  const data = Plotly.d3.csv.parse(csvContent);
-  return data;
+    Plotly.newPlot('line-chart-2', [trace], layout);
+  });
 }
 
-// 处理数据并绘制折线图
-async function createLineChart() {
-  const data = await loadData();
-
-  const x = data.map(row => row.Year);
-  const y = data.map(row => +row['Southern White Rhino population (AfRSG & other sources, 2019)']);
-
-  const trace = {
-    x: x,
-    y: y,
-    mode: 'lines+markers',
-    line: {
-      color: '#7c7c7c'
-    },
-    marker: {
-      color: '#333',
-      size: 6
-    },
-    name: 'Southern White Rhino population'
-  };
-
-  const layout = {
-    title: 'Southern White Rhino Population',
-    xaxis: {
-      title: 'Year'
-    },
-    yaxis: {
-      title: 'Population'
-    }
-  };
-
-  Plotly.newPlot('line-chart-2', [trace], layout);
-}
-
-// 调用函数绘制折线图
+// Call the function to create the line chart
 createLineChart();
